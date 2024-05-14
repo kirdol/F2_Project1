97320e8634f8c18ad61e5dfcefc71cf96476af2f
Lae-lae <79550430+Lae-lae@users.noreply.github.com>
2024-05-14 11:45:36 +0200

    changement

--- Toursim Final.R
+++ Toursim Final.R
@@ -10,6 +10,7 @@ lib("dplyr")
 lib("forecast")
 lib("fpp3")
 lib("tseries")
+lib("ggplot2")
 
 # Set the working directory
 wd <- dirname(rstudioapi::getSourceEditorContext()$path) 
@@ -42,6 +43,7 @@ head(vaud_data)
 # Check the structure of the filtered dataset
 str(vaud_data)
 
+
 ###--------Visualization-------------
 
 
@@ -63,13 +65,13 @@ ggplot(Yearly_totals, aes(x = Jahr, y = total_visitors_yearly, group = 1)) +
 monthly_totals <- vaud_data %>%
   group_by(Date) %>%
   summarise(total_visitors_monthly = sum(value), .groups = 'drop')
-plot(monthly_totals)
 
 #Decomposed the dataset 
 
-ts_data <- ts(monthly_totals$total_visitors_monthly, start = c(2005, 1), end = c(2023, 9),frequency = 12)  
+ts_data<- ts(monthly_totals$total_visitors_monthly, start = c(2005, 1), end = c(2023, 9),frequency = 12)  
 decomposed <- stl(ts_data, s.window = "periodic")
 plot(decomposed)
+
 
 # Fit a linear model to observe the trend
 trend_model_monthly <- lm(total_visitors_monthly ~ Date, data = monthly_totals)
@@ -100,33 +102,125 @@ ggAcf(ts_data)+ggtitle("Autocorrelation of a monthly visits")
 
 
 ###------------------Cleaning and Wrangling
+# Assign the original time series data (ts_data) to a new variable (ts_data_method_3)
+# This might be used for applying Method 3 on the time series data
+ts_data_method_3 <- ts_data
 
 
-#------- Check for outliers
-#tsouliers: 
-#index: This is a vector of integers indicating the positions in your time series where outliers have been detected.
-#replacements: These are the suggested replacement values for the outliers, which can be used to clean your data if you choose to remove or adjust the outliers.
+#-------------------------------------Method 1:  ts_outliers
 
+# Detect outliers in the time series data
 outliers <- tsoutliers(ts_data)
-ts_data[outliers$index] <- outliers$replacements
+
+# Get the indices of outliers 
+outlier_indices_to_replace <- outliers$index
+
+# Replace outliers 
+ts_data[outlier_indices_to_replace] <- outliers$replacements
+
+#--------------------------------------Method 2: dummy variables
+# Create a binary vector `r` with the same length as `ts_data`, filled with 1s.
+r <- rep(1, length(ts_data))
+
+# Set elements of `r` to 0 where the corresponding index of `ts_data` is greater than or equal to March 2020.
+r[which(index(ts_data) >= "2020-Mar")] <- 0
+
+# Update elements of `r` to 1 where the index of `ts_data` is greater than February 2021.
+r[which(index(ts_data) > "2021-Feb")] <- 1
+r
+
+
+#--------------------------------------Method 3: tsouliers + forecasting
+
+
+# Create a copy of ts_data with values from January 2015 to March-03 2020
+data_2015_2020 <- window(ts_data_method_3, start = c(2015, 1), end = c(2020, 2))
+
+# Fit ARIMA and ETS models to the 2015-2020 data and generate forecasts for 2020
+fit_arima <- auto.arima(data_2015_2020)  # Automatic fitting of an ARIMA model to the data spanning 2015 to 2020.FEB.
+fit_ets <- ets(data_2015_2020)  # Fitting an ETS (Exponential Smoothing State Space Model) to the same data.
+
+forecast_2020_arima <- forecast(fit_arima, h = 12)  # Generating a forecast for 2020 using the ARIMA model.
+forecast_2020_ets <- forecast(fit_ets, h = 12)  # Generating a forecast for 2020 using the ETS model.
+
+AIC(fit_arima) #1041.518
+BIC(fit_arima)#1045.342
+
+AIC(fit_ets)#1367.589
+BIC(fit_ets)#1403.751
+
+
+summary(forecast_2020_arima)  # Summarizing the forecast results from the ARIMA model.
+summary(forecast_2020_ets)  # Summarizing the forecast results from the ETS model.
+
+# Extracting fitted values for 2020 from the forecast
+fitted_values_2020 <- forecast_2020_arima$mean  # Extracting the mean fitted values for 2020 from the forecast object.
+print(fitted_values_2020)  # Printing the extracted fitted values.
+
+
+# Find the starting and ending indices for the range to be replaced
+start_index <- 183
+end_index <- 194
+
+# Replace the values in the ts_data_method_3 time series from start_index to end_index with the fitted values for the year 2020.
+# This is likely part of a process to update or correct the time series with specific fitted values.
+ts_data_method_3[start_index:end_index] <- fitted_values_2020
+
+# Display or print the updated time series data stored in ts_data_method_3.
+ts_data_method_3
+
+
+# Detect outliers in the time series data
+outliers <- tsoutliers(ts_data_method_3)
+
+# Get the indices of outliers 
+outlier_indices_to_replace <- outliers$index
+
+# Replace outliers 
+ts_data_method_3[outlier_indices_to_replace] <- outliers$replacements
+
+
 
 ###--------------- Modeling -----------
 #Fit Naive method
 fit_naive <- naive(ts_data , h=15)
-summary(ts_data) #Residual sd: 28476.6024 
+fit_naive_method3 <- naive(ts_data_method_3, h=15)
+summary(fit_naive) #Residual sd: 28476.6024 
+summary(fit_naive_method3) #28599.9411 
 
 ## Fit SNaive method
 fit_snaive <- snaive(ts_data , h=15)
-summary(fit_naive) #Residual sd: Residual sd: 25572.8135
+fit_snaive_method3 <- snaive(ts_data_method_3, h=15)
+summary(fit_snaive) #Residual sd: Residual sd: 25572.8135---- 28074.0992 
+summary(fit_snaive_method3) #16701.6654 
 
 
-#fit ETS method
+
+# Fit an ARIMA model to the time series data (ts_data) using the external regressors (xreg) stored in the variable r.
+# The variable r contains dummy variables where 0 represents periods affected by COVID-19 (marked as outliers)
+# and 1 represents non-COVID-19 periods.
+# The auto.arima function automatically selects the best ARIMA model based on AIC/BIC criteria, incorporating the effects of the dummy variables.
+arima_dummy <- auto.arima(ts_data, xreg = r)
+
+# Forecast for the next 15 months
+forecast_arima <- forecast(arima_dummy, xreg = rep(1, 15))
+
+# Print the forecasted values, ARIMA(2,0,2)(0,1,1)[12] 
+summary(forecast_arima) # residual s.d 9066.637
+
+
+#---------------fit ETS method
 fit_ets <- ets(ts_data)#ETS(A,N,A), residual sd: 9667.688
+fit_ets_method_3 <- ets(ts_data_method_3)#ETS(M,Ad,M)  #residual sd 0.0441
 summary(fit_ets)
+summary(fit_ets_method_3) 
 
-#fit Arima : Best model: ARIMA(2,0,2)(0,1,1)[12] with drift   
+
+#fit Arima : Best model: ARIMA(2,0,2)(0,1,1)[12] with drift for fit_arima  , ARIMA(3,0,1)(0,1,1)[12] with drift for fit_arima_method_3
 fit_arima<- auto.arima(ts_data,trace = T, stepwise = FALSE, approximation = FALSE)
+fit_arima_method_3 <- auto.arima(ts_data_method_3,trace = T, stepwise = FALSE, approximation = FALSE)
 summary(fit_arima)#residual sd: 9117.477
+summary(fit_arima_method_3) #residual sd:8611.221
   
 #-------Forecast and Validation ---------
 
@@ -135,39 +229,43 @@ forecast_snaive <- forecast(fit_snaive, h = 15)
 forecast_ets <- forecast(fit_ets, h=15,level=95)
 forecast_arima <- forecast(fit_arima, h=15, level=95)
 
+forecast_naive_method_3<- forecast(fit_naive_method3, h = 15)
+forecast_snaive_method_3 <- forecast(fit_snaive_method3, h = 15)
+forecast_ets_method_3 <- forecast(fit_ets_method_3, h=15,level=95)
+forecast_arima_method_3 <- forecast(fit_arima_method_3, h=15, level=95)
+
 accuracy(forecast_naive )
 accuracy(forecast_snaive)
 accuracy(forecast_ets)
 accuracy(forecast_arima)
 
-AIC(fit_ets)# 5363.613
-BIC(fit_ets)#5414.855
 
+accuracy(forecast_naive_method_3 )
+accuracy(forecast_snaive_method_3)
+accuracy(forecast_ets_method_3)
+accuracy(forecast_arima_method_3)
+
+
+AIC(fit_ets) #5363.613
 AIC(fit_arima)#4505.204
+
+BIC(fit_ets) #5414.855
 BIC(fit_arima)#4528.733
 
-#----------------------weighted average 
-# Define weights for each model (e.g., based on performance or domain knowledge)
-weight_arima <- 0.9
-weight_ets <- 0.1
 
-# Combine forecasts using weighted average
-combined_forecast_values <- (weight_arima * fit_arima$fitted) + (weight_ets * fit_ets$fitted)
+AIC(fit_ets_method_3) # 5330.26
+AIC(fit_arima_method_3) #4496.834
 
-# Create a forecast object with the combined forecast values
-combined_forecast <- forecast(combined_forecast_values, h= 15)
-
-# Plot the combined forecast
-autoplot(combined_forecast)
-
-print(summary(combined_forecast )) # residual sd:  6946.932, AIC : 5214.893, BIC: 5266.135
+BIC(fit_ets_method_3) #5391.75
+BIC(fit_arima_method_3) #4520.363
 
 
-#Generally, you want to select the model with the lowest AIC and BIC values, as these criteria balance model fit and complexity.
-#Additionally, a lower residual SD indicates a better fit of the model to the data. ---> ARIMA. 
 
 #--------- Choose Arima---------------
 
+
+print(forecast_arima_method_3, level=95)
+
 # Plot the forecast with confidence intervals
-autoplot(forecast_ets)+autoplot(ts_data)
+autoplot(forecast_arima_method_3)+ autolayer(ts_data)
 
