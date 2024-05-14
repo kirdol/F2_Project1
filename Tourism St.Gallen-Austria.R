9cc5c8a2ece28187b7703c0f2cd7a42b506a59d5
dfariaca <fariacarvalhodaniela@gmail.com>
2024-05-15 01:17:20 +0200

    Continue
    More attempts: include dummy variable, mix, add some details ecc

--- Tourism St.Gallen-Austria.R
+++ Tourism St.Gallen-Austria.R
@@ -8,6 +8,9 @@ library("ggplot2")
 library("tsibble")
 library("forecast")
 library("ggseas")
+library("fpp2")
+library("timeSeries")
+#library("modeltime")
 
 
 # Set working directory
@@ -45,28 +48,35 @@ head(sg_data)
 # Structure of the dataset
 str(sg_data) #Value as numerical variable and Jahr as character
 
+#Create Time series, include start month
+ts_sg_data <- ts(sg_data$value, start = c(2005, 1), frequency = 12)
+ts_sg_data
+
 ##--------------- Visualizations: Year plot -----------
 # Aggregate data to have total number of visitors from Austria
 year_sg <- sg_data %>%
   group_by(Jahr) %>%
   summarise(tot_tourists_sg_au = sum(value), .groups = 'drop')
 
+# Create time series on yearly data
+ts_sg_year_data <- ts(year_sg$tot_tourists_sg_au, start = 2005, frequency = 1)
+ts_sg_year_data
+
 # Plot to visualize year data
-ggplot(year_sg, aes(x = Jahr, y = tot_tourists_sg_au, group = 1 )) +
-  geom_line()+
-  labs(x = 'Year', y = 'Number of Tourists', title = 'Total visitors in St.Gallen from Austria by year')+
-  theme_minimal()
+autoplot(ts_sg_year_data)+
+  ggtitle("Number of tourists per year") +
+  ylab("Number of tourists")
 
 ##--------------- Visualizations: Monthly plot -----------
 # Plot monthly data
-ggplot(sg_data, aes(x = Date, y = value, group = 1)) +
-  geom_line() +
-  labs(x = 'Year', y = 'Number of Tourists', title = 'Total visitors in St.Gallen from Austria by month') +
-  theme_minimal()
+autoplot(ts_sg_data)+
+  ggtitle("Number of tourists per month") +
+  ylab("Number of tourists")
+# We can see there is almost no trend, but huge seasonality
 
 ##--------------- Monthly trend and seasonality analysis (STL) -----------
+
 #use STL decomposition to see the trend and seasonality more clearly
-ts_sg_data <- ts(sg_data$value, frequency = 12)
 decomposition_sg <- stl(ts_sg_data, s.window = "periodic")
 plot(decomposition_sg, main = "STL Decomposition of Tourism time series")
 
@@ -75,8 +85,18 @@ plot(decomposition_sg, main = "STL Decomposition of Tourism time series")
 #The trend can be seen as "stable" the only outlier is the COVID period (2020-21)
 #Reminders seem white noise which is good
 
-#Seasonal view
-seasonplot(ts_sg_data, main = "Seasonal Plot of Tourism Time Series")
+# Create graph seasonality
+ggseasonplot(ts_sg_data) + 
+  ggtitle("SG-AU seasonality analysis") +
+  ylab("Number of Tourists")
+
+# There is a strong seasonality as stated in the STL Decomposition
+# COVID year is clearly visible
+
+#Subseries plot
+ggsubseriesplot(ts_sg_data) +
+  ggtitle("Subseries analysis") +
+  ylab("Number of Tourists")
 
 ##--------------- Outliers analysis ---------------------------------
 outliers_sg <- tsoutliers(ts_sg_data)
@@ -87,11 +107,32 @@ ts_sg_data_clean[outliers_sg$index] <- outliers_sg$replacements #replace outlier
 plot(ts_sg_data, main = "SG-AU time series", ylab = "Number of visitors")
 plot(ts_sg_data_clean, main = "SG-AU after outlier replacement", ylab = "Number of visitors")
 
+
+##--------------- COVID-19 Dummy variable ---------------------------------
+#Create dummy variable to manage the COVID-19 period
+start_date <- as.Date("2020-03-01")
+end_date <- as.Date("2021-04-30")
+sg_data$sg_covid_dummy <- 1  # Initiate dummy variable at 1
+sg_data$sg_covid_dummy[sg_data$Date >= start_date & sg_data$Date <= end_date] <- 0  # Specified COVID period
+
+sg_covid <- ts(sg_data$sg_covid_dummy, start = c(2005, 1), frequency = 12)
+sg_covid
+
+#Implement dummy variable on time series
+#ts_sg_data_covid <- cbind(ts_sg_data, sg_covid)
+#ts_sg_data_covid
+
 ##--------------- First Forecast analysis: Naive, S-Naive (DA RIVEDERE il plot) -----------
 #Create models to forecast
-Mean_sg <- meanf(ts_sg_data_clean, h=15)
-Naive_sg <- naive(ts_sg_data_clean, h=15)
-S_Naive_sg <- snaive(ts_sg_data_clean, h= 15)
+Mean_sg <- meanf(ts_sg_data)
+summary(Mean_sg)
+checkresiduals(Mean_sg)
+
+Naive_sg <- naive(ts_sg_data)
+summary(Naive_sg)
+
+S_Naive_sg <- snaive(ts_sg_data)
+summary(S_Naive_sg) 
 
 # Plot graphs of forecasts
 # 1. Mean
@@ -119,10 +160,17 @@ legend("topright", legend = c("Mean", "Naive", "S-Naive"), col = c("lightblue", 
 #Naive model takes more into consideration the level of last observation
 #S-Naive takes into consideration the seasonality
 
+#mean_model<- ts_sg_data %>% model(
+  #Mean = mean(ts_sg_data),
+  #Naive = naive(ts_sg_data),
+  #S_Naive = snaive(ts_sg_data))
+
+
+
 
 ##--------------- Second Forecast analysis: ARIMA -----------
 # Select best arima from auto.arima function
-arima_sg_model <- auto.arima(ts_sg_data_clean, seasonal = TRUE, stepwise = TRUE, approximation = FALSE)
+arima_sg_model <- auto.arima(ts_sg_data, seasonal = TRUE, stepwise = TRUE, approximation = FALSE)
 
 #Summary of the ARIMA model created
 summary(arima_sg_model)
@@ -134,7 +182,7 @@ plot(forecast_arima_sg, main = "Forecast ARIMA")
 ##--------------- Third Forecast analysis: ETS -----------
 
 #First try an SES (no trend and no seasonality)
-ets_model_1 <- ets(ts_sg_data_clean, model = "ANN")
+ets_model_1 <- ets(ts_sg_data, model = "ANN")
 summary(ets_model_1)
 
 # Compute forecast and plot the result
@@ -142,7 +190,7 @@ forecast_ets_1 <- forecast(ets_model_1, h = 15)
 plot(forecast_ets_1, main = "Forecast using ETS(A,N,N)", ylab = "Number of tourists")
 
 # Try to see which ETS is better to use
-ets_model_best <- ets(ts_sg_data_clean, model = "ZZZ")
+ets_model_best <- ets(ts_sg_data, model = "ZZZ")
 summary(ets_model_best) #ETS(A,N,A) is suggested as actually there is no big trend but great seasonality (additive)
 
 # Compute forecast of ETS (A,N,A) and plot the result
@@ -157,6 +205,8 @@ BIC(arima_sg_model) # 2634
 AIC(ets_model_best) # 3341
 BIC(ets_model_best) # 3392
 
+# cross-validation
+
 # Question: ETS or ARIMA?? a little bit confused... advices?
 # Looking at the RMSE (we want the lower) ETS(A,N,A) looks slightly better than the ARIMA model
 accuracy(forecast_naive)
@@ -164,3 +214,72 @@ accuracy(forecast_snaive)
 accuracy(forecast_mean)
 # Interesting to see that looking at the results we have:
 # Naive > S-Naive > Mean forecast
+# Dummy variables?
+
+# ------------- Fit ARIMA model with the dummy variable ---------------------
+arima_model <- auto.arima(ts_sg_data, xreg = sg_covid) # Fit ARIMA model with the dummy variable as an exogenous regressor
+summary(arima_model)
+
+# Forecast for the next 15 months
+forecast_arima_dummy <- forecast(arima_model, xreg = rep(1, 15)) # Make a forecast for the next 15 months using the fitted ARIMA model
+
+# Print the forecast
+print(forecast_arima_dummy) # Print the forecasted values
+
+plot(forecast_arima_dummy, main = "Forecast of number of tourists using ARIMA model")
+checkresiduals(forecast_arima_dummy)
+
+# CROSS VALIDATION?!
+
+# ------------- ETS with dummy variable --------------------------
+# No direct support to insert an exogenous variable directly into the modeling
+# Need to estimate effect of exo variable on the ts before fitting ETS model
+# Estimate effect of the exogenous variable (here the dummy variable) on the time series using linear regression model
+lm_model_covid <- lm(ts_sg_data ~ sg_covid)
+summary(lm_model_covid)
+sg_covid_effect <- predict(lm_model_covid)
+
+# Subtract effect of the exogenous variable from the original time series
+ts_sg_data_adjusted_covid <- ts_sg_data - sg_covid_effect
+
+# Fit ETS model to the residual time series
+ets_model_covid <- ets(ts_sg_data_adjusted_covid, model = "ZZZ")
+summary(ets_model_covid) #ETS (A,N,A)
+
+# Forecast for the next 15 months
+forecast_ets_covid <- forecast(ets_model_covid, h = 15)
+
+# Print the forecast
+print(forecast_ets_covid)
+
+# Plot the forecast
+plot(forecast_ets_covid, main = "Forecast using ETS model with dummy variable")
+
+
+# ---------------- Accuracy with dummy ------------------
+accuracy(forecast_arima)
+accuracy(forecast_ets_covid)
+# better accuracy with dummy variable rather than outliers
+
+
+
+
+# ---------------- Outliers and dummy ---------------
+arima_model_outliers <- auto.arima(ts_sg_data_clean, xreg = sg_covid) # Fit ARIMA model with the dummy variable as an exogenous regressor
+summary(arima_model_outliers)
+forecast_arima_dummy_outliers <- forecast(arima_model_outliers, xreg = rep(1, 15)) # Make a forecast for the next 15 months using the fitted ARIMA model
+plot(forecast_arima_dummy_outliers)
+
+# --------------- ETS: high alpha and dummy -------
+ets_model_alpha <- ets(ts_sg_data_adjusted_covid, model = "ZZZ", alpha = 0.9)
+summary(ets_model_alpha) # No great results, RMSE still higher
+
+
+#-------------- TSLM ----------------------------
+# Compute correlation between number of visitors and covid
+correlation <- cor(ts_sg_data, sg_covid)
+print(correlation)
+
+# Create TSLM model
+tslm_model <- lm(ts_sg_data ~ sg_covid)
+summary(tslm_model)
